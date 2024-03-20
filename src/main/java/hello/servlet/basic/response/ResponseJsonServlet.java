2c676b916b4843b3283487145ff812d1379cbd0d
JEON-MIN-SEO <jmsparrowk@naver.com>
2024-03-20 16:23:36 +0900

    3/20

---
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setAge(20);
+        helloData.setUsername("jeon");
+
+        String result = objectMapper.writeValueAsString(helloData);
+
+    }
+}
