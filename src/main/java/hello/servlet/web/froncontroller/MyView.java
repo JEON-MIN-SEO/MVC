a2f80750629bff2eb60ff1661d0893d46611a5e5
JEON-MIN-SEO <jmsparrowk@naver.com>
2024-03-21 19:49:32 +0900

    v1

---
+++ src/main/java/hello/servlet/web/froncontroller/MyView.java
@@ -0,0 +1,11 @@
+package hello.servlet.web.froncontroller;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath){
+        this.viewPath = viewPath;
+    }
+
+
+}
---
+++ src/main/java/hello/servlet/web/froncontroller/v1/controller/MemberFromControllerV1.java
@@ -0,0 +1,15 @@
+package hello.servlet.web.froncontroller.v1.controller;
+
+import hello.servlet.web.froncontroller.v1.ControllerV1;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFromControllerV1 implements ControllerV1 {
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
