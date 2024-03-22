120127f601f74376bef887f286965ea90ffa8c03
JEON-MIN-SEO <jmsparrowk@naver.com>
2024-03-22 17:42:30 +0900

    v2

---
+++ src/main/java/hello/servlet/web/froncontroller/v1/controller/MyView.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.froncontroller.v1.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
