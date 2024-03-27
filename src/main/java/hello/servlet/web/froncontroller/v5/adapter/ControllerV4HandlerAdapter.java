33ff4a7b7eab567bd798988318aa33394241d86d
JEON-MIN-SEO <jmsparrowk@naver.com>
2024-03-27 17:35:57 +0900

    3.27

--- src/main/java/hello/servlet/web/froncontroller/v5/adapter/ControllerV4HandlerAdapter.java
+++ src/main/java/hello/servlet/web/froncontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -1,21 +1,40 @@
 package hello.servlet.web.froncontroller.v5.adapter;
 
 import hello.servlet.web.froncontroller.ModelView;
+import hello.servlet.web.froncontroller.v3.ControllerV3;
+import hello.servlet.web.froncontroller.v4.ControllerV4;
 import hello.servlet.web.froncontroller.v5.MyHandlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
     @Override
     public boolean supports(Object handler) {
-        return false;
+        return (handler instanceof ControllerV4);
     }
 
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
-        return null;
+        ControllerV4 controller =(ControllerV4) handler;
+
+        Map<String, String> paramMap = createParamMap(request); //파라미터 값을 모두 뽑는 작업
+        Map<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
+        return mv;
     }
+
+    private static Map<String, String> createParamMap(HttpServletRequest req) {
+        Map<String, String > paramMap = new HashMap<>();
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, req.getParameter(paramName)));
+        return paramMap;
 }
